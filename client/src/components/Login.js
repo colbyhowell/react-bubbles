46a4d6840ca94613765f8e1fee989b5cda10937f
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-10-11 20:27:02 -0500

    got the bubble functionality displaying

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,43 @@
 import React from "react";
+import useForm from "react-hook-form";
+import axiosWithAuth from "../utils/axiosWithAuth.js";
+import { useHistory } from "react-router-dom";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const { handleSubmit, register, errors } = useForm();
+  const history = useHistory();
+
+  const onSubmit = values => {
+    axiosWithAuth()
+      .post("/login", values)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        history.push("/bubbles");
+      })
+      .catch(err => console.log(err));
+  };
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <br />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          ref={register}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          ref={register}
+        />
+        <input type="submit" />
+      </form>
     </>
   );
 };
